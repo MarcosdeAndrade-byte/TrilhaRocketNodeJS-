c20b822af24174b0bc4da8d05b946a000134f0ae
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-10-13 10:41:24 -0300

    Adicionando testes de autenticacao do usuario - Usuario nao existente e senha incorreta

--- src/modules/accounts/authenticateUser/authenticateUserUseCase.spec.ts
+++ src/modules/accounts/authenticateUser/authenticateUserUseCase.spec.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { AppError } from '../../../errors/AppError';
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { UsersRepositoryInMemory } from '../repositories/in-memory/userRepositoryInMemory';
 import { CreateUserUseCase } from '../useCases/CreateUser/CreateUserUseCase';
@@ -38,5 +39,42 @@ describe('Authenticate User', () => {
         });
 
         expect(result).toHaveProperty('token');
+    });
+
+    it('should not be able to authenticate an nonexistent user', () => {
+        // Quando você está escrevendo testes, muitas das vezes você precisa checar se
+        // os valores satisfazem certas condições. expect lhe dá acesso a inúmeros
+        // "matchers" que permitem validar diferentes coisas.
+        expect(async () => {
+            await authenticateUserUseCase.execute({
+                email: 'false@email.com',
+                password: '1234',
+            });
+        }).rejects.toBeInstanceOf(AppError);
+        // Como esperado o teste deve falhar
+    });
+
+    it('should not be able to authenticate with incorrect password', () => {
+        expect(async () => {
+            // Criamos uma instância do usuário
+            const user: ICreateUserDTO = {
+                driver_license: '00123',
+                email: 'user@user.com',
+                password: '1234',
+                name: 'User Test Error',
+                username: '',
+                isAdmin: false,
+                id: '',
+            };
+
+            // Criamos ele no banco de dados
+            await createUserUseCase.execute(user);
+
+            // Passamos um email válido para uma senha incorreta
+            await authenticateUserUseCase.execute({
+                email: user.email,
+                password: 'incorrectPassword',
+            });
+        }).rejects.toBeInstanceOf(AppError);
     });
 });
